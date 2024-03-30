5ffbbc029410287ab7fea8d64ea8cc5da552eccd
GwinProGrammer <gwinthepro@gmail.com>
2024-03-30 17:17:25 -0400

    compiles

--- hw3/hw3.c
+++ hw3/hw3.c
@@ -18,59 +18,60 @@ typedef struct {
     int moves_made;
 } shared_data;
 
-int generate_knight(int** board, int x, int y, int** moves){
+int generate_knight(int** board, int x, int y, int moves[8][2]){
     
     int possible_moves = 0;
-    int** moves = calloc(8,sizeof(int*));
+
 
     if (x - 1 > 0 && y - 2 > 0 && board[x-1][y-2] == 0){
-        moves[possible_moves] = calloc(2,sizeof(int));
+
         moves[possible_moves][0] = x-1;
         moves[possible_moves][1] = y-2;
         possible_moves++;
     }
     if (x - 2 > 0 && y - 1 > 0 && board[x-2][y-1] == 0){
-        moves[possible_moves] = calloc(2,sizeof(int));
+
         moves[possible_moves][0] = x-2;
         moves[possible_moves][1] = y-1;
         possible_moves++;
     }
     if (x - 2 > 0 && y +1 < n && board[x-2][y+1] == 0){
-        moves[possible_moves] = calloc(2,sizeof(int));
+
         moves[possible_moves][0] = x-2;
         moves[possible_moves][1] = y+1;
         possible_moves++;
     }
     if (x - 1 > 0 && y +1 < n && board[x-1][y+2] == 0){
-        moves[possible_moves] = calloc(2,sizeof(int));
+
         moves[possible_moves][0] = x-1;
         moves[possible_moves][1] = y+2;
         possible_moves++;
     }
     if (x + 1 < m && y + 2 < n && board[x+1][y+2] == 0){
-        moves[possible_moves] = calloc(2,sizeof(int));
+
         moves[possible_moves][0] = x+1;
         moves[possible_moves][1] = y+2;
         possible_moves++;
     }
     if (x +2 < m && y +1 < n && board[x+2][y+1] == 0){
-        moves[possible_moves] = calloc(2,sizeof(int));
+
         moves[possible_moves][0] = x+2;
         moves[possible_moves][1] = y+1;
         possible_moves++;
     }
     if (x +2 < m && y - 1 > 0 && board[x+2][y-1] == 0){
-        moves[possible_moves] = calloc(2,sizeof(int));
+
         moves[possible_moves][0] = x+2;
         moves[possible_moves][1] = y-1;
         possible_moves++;
     }
     if (x + 1 < m && y - 2 > 0 && board[x+1][y-2] == 0){
-        moves[possible_moves] = calloc(2,sizeof(int));
+
         moves[possible_moves][0] = x+1;
         moves[possible_moves][1] = y-2;
         possible_moves++;
     }
+    
     return possible_moves;
 }
 
@@ -96,12 +97,14 @@ void free_moves(int** moves,int num_moves){
 
 void *tour(void *arg){
     shared_data *data = (shared_data *)arg;
+    printf("tour %d\n", data->moves_made);
     
-    int** moves;
-    int num_moves;
     while(1){
+        int moves[8][2];
+        int num_moves;
+        printf("gay4\n");
        num_moves = generate_knight(data->board,data->knight_x,data->knight_y,moves);
-
+        printf("gay5\n");
        if (num_moves == 1){
         int new_x = moves[0][0];
         int new_y = moves[0][1];
@@ -109,25 +112,30 @@ void *tour(void *arg){
         data->knight_x = new_x;
         data->knight_y = new_y;
         data->moves_made += 1;
-        free_moves(moves,num_moves);
+        // free_moves(moves,num_moves);
        }
-
+       
        else if (num_moves > 1){
 
-        printf("%d possible moves after move %d", num_moves,data->moves_made);
+        printf("%d possible moves after move %d\n", num_moves,data->moves_made);
         //create new array to keep track of all the threads
         pthread_t threads[num_moves];
 
         for(int i = 0; i < num_moves; i++){
+            printf("gay6\n");
             int** new_board = copy_board(data->board);
+            printf("gay7\n");
             int new_x = moves[i][0];
+            printf("gay7.5\n");
             int new_y = moves[i][1];
+            printf("gay8\n");
             new_board[data->knight_x][data->knight_y] = 1;
             shared_data *new_data = calloc(1,sizeof(shared_data));
             new_data->board = new_board;
             new_data->knight_x = new_x;
-            new_data->knight_y - new_y;
+            new_data->knight_y = new_y;
             new_data->moves_made = data->moves_made + 1;
+            
             //create new thread
             pthread_create(&threads[i], NULL, tour, (void*)new_data);
 
@@ -137,16 +145,23 @@ void *tour(void *arg){
                 printf("Thread %d has finished\n", i + 1);
             }
         }
+
         if (!no_parallel){
             for (int i = 0; i < num_moves; i++) {
                 pthread_join(threads[i], NULL);
                 printf("Thread %d has finished\n", i + 1);
             }
         }
-        free_moves(moves,num_moves);
+        // free_moves(moves,num_moves);
+        unsigned int *y = malloc(sizeof(*y));
+        *y = pthread_self();
+        pthread_exit(y);
        }
        else{
         printf("dead end\n");
+        unsigned int *y = malloc(sizeof(*y));
+        *y = pthread_self();
+        pthread_exit(y);
        }
     }
     
@@ -165,6 +180,7 @@ void *tour(void *arg){
 }
 
 int main() {
+    printf("start\n");
     int** new_board = calloc(m,sizeof(int*));
     for (int i = 0; i < m; i++){
         int* new_row = calloc(n, sizeof(int));
@@ -177,15 +193,18 @@ int main() {
     new_data->knight_y = 0;
     new_data->moves_made=0;
 
+    printf("gay2\n");
+
     pthread_t thread;
     pthread_create(&thread, NULL, tour, (void*)new_data);
     
-    if(no_parallel){
-        pthread_join(thread, NULL);
-        printf("Thread has finished\n");
-    }
+    printf("gay3\n");
 
-    return(EXIT_FAILURE);
+    pthread_join(thread, NULL);
+    printf("Thread has finished\n");
+    printf("gay4\n");
+
+    return(EXIT_SUCCESS);
 }
 
 
